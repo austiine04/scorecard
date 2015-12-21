c23bc9bb3eb1cf6280917b26e6bafb9025ef2c4b
austiine04 <austiine04@gmail.com>
2015-12-21 18:55:42 +1100

    flat ui icon fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,45 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# flat ui glyphicons
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/glyphicons/flat-ui-icons-regular.eot )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/glyphicons/flat-ui-icons-regular.svg )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/glyphicons/flat-ui-icons-regular.ttf )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/glyphicons/flat-ui-icons-regular.wof )
+
+# flat ui icon fonts lato-black
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-black.eot )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-black.svg )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-black.ttf )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-black.woff )
+
+# flat ui icon fonts lato-bold
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bold.eot )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bold.svg )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bold.ttf )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bold.woff )
+
+# flat ui icon fonts lato-bolditalic
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bolditalic.eot )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bolditalic.svg )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bolditalic.ttf )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-bolditalic.woff )
+
+# flat ui icon fonts lato-italic
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-italic.eot )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-italic.svg )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-italic.ttf )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-italic.woff )
+
+# flat ui icon fonts lato-light
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-light.eot )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-light.svg )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-light.ttf )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-light.woff )
+
+# flat ui icon fonts lato-regular
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-regular.eot )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-regular.svg )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-regular.ttf )
+Rails.application.config.assets.precompile += %w( flat-ui/dist/fonts/lato/lato-regular.woff )
